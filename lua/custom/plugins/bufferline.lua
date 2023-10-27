f8479be7903b2881297f619f44cd83ac958777a5
Miguelo Sana <miguelosana@gmail.com>
2023-10-27 22:43:44 +0200

    update bufferline and conform

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -1,18 +1,22 @@
 -- Buffer line
 return {
-	'akinsho/bufferline.nvim',
-	version = "*",
-	dependencies = 'nvim-tree/nvim-web-devicons',
-	config = function()
-		require("bufferline").setup({
-			options = {
-				style_preset = 2,
-				mode = "buffers",
+  'akinsho/bufferline.nvim',
+  version = '*',
+  dependencies = 'nvim-tree/nvim-web-devicons',
+  lazy = false,
+  keys = {
+    { '<leader>bl', '<cmd>BufferLinePick<cr>', desc = '[B]uffer[L]inePick' },
+    { '<leader>bc', '<cmd>BufferLinePickClose<cr>', desc = '[B]ufferLinePick[C]lose' },
+  },
+  config = function()
+    require('bufferline').setup {
+      options = {
+        style_preset = 2,
+        mode = 'buffers',
 
-				separator_style = "slant",
-				diagnostics = "nvim_lsp",
-			}
-
-		})
-	end
+        separator_style = 'slant',
+        diagnostics = 'nvim_lsp',
+      },
+    }
+  end,
 }
--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -1,24 +1,24 @@
 return {
-	'stevearc/conform.nvim',
-	event = { "BufWritePre" },
-	cmd = { "ConformInfo" },
-	keys = {
-		{
-			"<leader>f",
-			function()
-				require("conform").format({ async = true, lsp_fallback = true })
-			end,
-			mode = "",
-			desc = "Format Buffer",
-		},
-	},
-	opts = {
-		formatters_by_ft = {
-			lua = { "stylua" },
-			python = { "yapf" },
-			javascript = { "prettier" },
-		},
-		format_on_save = { timeout_ms = 500, lsp_fallback = false },
-
-	}
+  'stevearc/conform.nvim',
+  event = { 'BufWritePre' },
+  cmd = { 'ConformInfo' },
+  keys = {
+    {
+      '<leader>f',
+      function()
+        require('conform').format { async = true, lsp_fallback = true }
+      end,
+      mode = '',
+      desc = 'Format Buffer',
+    },
+  },
+  opts = {
+    formatters_by_ft = {
+      lua = { 'stylua' },
+      python = { 'yapf' },
+      javascript = { 'prettier' },
+      go = { 'gofmt' },
+    },
+    format_on_save = { timeout_ms = 500, lsp_fallback = false },
+  },
 }
